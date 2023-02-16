b3a2e3b0134a3007e82bba4f73d6b11f2db54917
Mark Jay Delos Santos <marksantos002219@gmail.com>
2023-02-16 12:30:08 +0800

    fix2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   url: 'hypersaver-drugstore.web.app',
-  apiUrl: 'https://pos-server-vq2z.onrender.com'
+  apiUrl: 'https://pos-server-vq2z.onrender.com/api'
 };
