34bed3ea9e3c8d6f6a47e8cca1b8324f36f4a083
Mark Jay Delos Santos <marksantos002219@gmail.com>
2023-03-29 13:16:13 +0800

    Removed all filters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import config from 'devextreme/core/config';
+import DataGrid, { Properties } from 'devextreme/ui/data_grid';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   constructor() {
-    config({ defaultCurrency: 'PHP' })
+    config({ defaultCurrency: 'PHP' });
     // themes.current('generic.dark');
+    const options: Properties = {
+      searchPanel: {
+        visible: true
+      },
+      headerFilter: {
+        visible: false
+      },
+      filterRow: {
+        visible: false
+      }
+    }
+    DataGrid.defaultOptions({
+      options: {
+        searchPanel: {
+          visible: true,
+        },
+      }
+    });
   }
 }
