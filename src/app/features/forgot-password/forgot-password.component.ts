2b0392af91c0bfbeed891fb2a7f7432d5dbbc6bd
Mark Jay Delos Santos <marksantos002219@gmail.com>
2023-04-02 14:38:18 +0800

    removed firebase

--- src/app/features/forgot-password/forgot-password.component.ts
+++ src/app/features/forgot-password/forgot-password.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { DxFormComponent } from 'devextreme-angular';
 import notify from 'devextreme/ui/notify';
 
@@ -15,24 +14,24 @@ export class ForgotPasswordComponent implements OnInit {
 
   loadingVisible = false;
 
-  constructor(private _auth: AngularFireAuth) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
 
-  submit(form: DxFormComponent): void {
-    const validationResult = form.instance.validate();
-    if (!validationResult.isValid) return;
-    const formData = form.instance.option('formData');
-    this.loadingVisible = true;
-    this._auth.sendPasswordResetEmail(formData.email)
-      .then((res) => {
-        notify(`Password reset link is successfully sent to ${formData.email}.`, 'success', 5000);
-      })
-      .catch(() => {
-        notify('There is no user record corresponding to this identifier. The user may have been deleted.', 'error', 5000);
-      })
-      .finally(() => this.loadingVisible = false);
-  }
+  // submit(form: DxFormComponent): void {
+  //   const validationResult = form.instance.validate();
+  //   if (!validationResult.isValid) return;
+  //   const formData = form.instance.option('formData');
+  //   this.loadingVisible = true;
+  //   this._auth.sendPasswordResetEmail(formData.email)
+  //     .then((res) => {
+  //       notify(`Password reset link is successfully sent to ${formData.email}.`, 'success', 5000);
+  //     })
+  //     .catch(() => {
+  //       notify('There is no user record corresponding to this identifier. The user may have been deleted.', 'error', 5000);
+  //     })
+  //     .finally(() => this.loadingVisible = false);
+  // }
 
 }
